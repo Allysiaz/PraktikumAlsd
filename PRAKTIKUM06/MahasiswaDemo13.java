8bf8544daa1f7f3a491362530b58ef4b96d58781
Allysiaz <juliettanadine@gmail.com>
2025-03-24 01:08:29 +0800

    PRAKTIKUM06

---
+++ PRAKTIKUM06/MahasiswaDemo13.java
@@ -0,0 +1,44 @@
+package PRAKTIKUM06;
+import java.util.Scanner;
+public class MahasiswaDemo13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = sc.nextInt();
+        sc.nextLine();
+
+        MahasiswaBerprestasi13 list = new MahasiswaBerprestasi13(jumlahMahasiswa);
+
+        for (int i = 0; i < jumlahMahasiswa; i++) {
+            System.out.println("Masukkan data mahasiswa ke-" + (i + 1));
+            System.out.print("NIM: ");
+            String nim = sc.nextLine();
+            System.out.print("Nama: ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas: ");
+            String kelas = sc.nextLine();
+            System.out.print("IPK: ");
+            double ipk = sc.nextDouble();
+            sc.nextLine();
+
+            list.tambah(new Mahasiswa13(nim, nama, kelas, ipk));
+        }
+
+        System.out.println("Data mahasiswa sebelum sorting : ");
+        list.tampil();
+
+        System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC) : ");
+        list.bubbleSort();
+        list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC)");
+        list.selectionSort();
+        list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC)");
+        list.insertionSort();
+        list.tampil();
+    
+    }
+}
