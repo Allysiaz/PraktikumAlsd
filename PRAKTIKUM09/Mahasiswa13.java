8638cdd23111ec8c97206925f9fc46d916871ec5
Allysiaz <juliettanadine@gmail.com>
2025-04-30 18:06:45 +0800

    PRAKTIKUM09

---
+++ PRAKTIKUM09/Mahasiswa13.java
@@ -0,0 +1,22 @@
+public class Mahasiswa13 {
+
+    String nim;
+    String nama;
+    String kelas;
+    int nilai;
+
+    Mahasiswa13(){
+
+    }
+
+    Mahasiswa13(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai (int nilai) {
+        this.nilai = nilai;        
+    }
+}
